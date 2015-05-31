3e1395434468933a58f61686e9b5f30bc2033325
Andrew Pritchard <awpritchard@gmail.com>
2015-05-31 14:22:51 -0700

    Add support for tones, change main to generate a melodic minor scale with harmonic overtones

--- System.hs
+++ System.hs
@@ -1,8 +1,10 @@
 {-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TupleSections #-}
 module System where
 
 import Prelude hiding (writeFile)
 
+import           Control.Arrow (first, second)
 import           Data.Ratio (Ratio, (%))
 import           Data.Int (Int16)
 
@@ -33,28 +35,65 @@ pythagorean p = System
     , scale_ = [1, 256%243, 9%8, 32%27, 81%64, 4%3, 729%512, 3%2, 128%81, 27%16, 16%9, 243%128]
     }
 
-type Note = Int
+type Note = (Int, Tone)
 
 data Composition = Composition
     { system_ :: System
     , notes_  :: [Note]
     }
 
-ionian :: Composition
-ionian = Composition
-    { system_ = pythagorean 440
-    , notes_  = [0, 2, 4, 5, 7, 9, 11, 12]
+ionian :: Pitch -> Composition
+ionian p = Composition
+    { system_ = pythagorean p
+    , notes_  = map (,fifthed) [0, 2, 4, 5, 7, 9, 11, 12]
     }
 
-type Sequencing = [Fragment]
+melodicMinor :: Pitch -> Composition
+melodicMinor p = Composition
+    { system_ = pythagorean p
+    , notes_  = map (,harmonic)
+        [ 0, 0, 2, 3, 5, 7, 9, 11, 12
+        , 12, 10, 8, 7, 5, 3, 2, 0, 0]
+    }
+
+type Spectrum = [(Pitch, Double)]
+type Tone = Pitch -> Spectrum
+
+synthesizeSpectrum :: Spectrum -> Waveform
+synthesizeSpectrum =
+    foldl addWaveform (const 0) .
+    map (uncurry (flip scaleWaveform) . first pitchWaveform)
+
+octaved :: Tone
+octaved p = [(p, 0.5), (2*p, 0.2)]
+
+harmonic :: Tone
+harmonic p = map (second (/2)) [(p, 0.5), (2*p, 0.2), (3*p, 0.1), (4*p, 0.05), (5*p, 0.025)]
+
+fifthed :: Tone
+fifthed p = [(p, 0.3), (p * (3%2), 0.3), (p * 2, 0.15)]
+
+type Waveform = Rational -> Double
+
+type Sequencing = [(Waveform, Int)]
 
 pitch :: System -> Int -> Pitch
 pitch (System f c s) n = f * (c ^ fromIntegral octave) * (s !! step)
     where (octave, step) = n `divMod` length s
 
+pitchWaveform :: Pitch -> Rational -> Double
+pitchWaveform p = sin . (2 * pi * fromRational p *) . fromRational
+
+addWaveform :: Waveform -> Waveform -> Waveform
+addWaveform wvl wvr = \t -> wvl t + wvr t
+
+scaleWaveform :: Double -> Waveform -> Waveform
+scaleWaveform f wv = (f*) . wv
+
 interp :: Composition -> Sequencing
 interp (Composition _ []    ) = []
-interp (Composition s (x:xs)) = Fragment (pitch s x) 0.1 22050 : interp (Composition s xs)
+interp (Composition s ((n, tone):xs)) =
+    (scaleWaveform 0.2 . synthesizeSpectrum . tone $ pitch s n, 11025) : interp (Composition s xs)
 
 type Sample = Int16
 
@@ -68,19 +107,9 @@ quantize x = floor $ toRational x * toRational scale / 2
         scale :: Integer
         scale = fromIntegral (maxBound :: b) - fromIntegral (minBound :: b)
 
-data Fragment = Fragment
-    { pitch_    :: Pitch
-    , amp_      :: Rational
-    , duration_ :: Int
-    }
-
-samplePitchAt :: SampleRate -> Pitch -> Int -> Double
-samplePitchAt rate p =
-    sin . (2 * pi * fromRational p *) . fromRational . (% rate) . fromIntegral
-
-synthesizeFragment :: SampleRate -> Fragment -> Vector Sample
-synthesizeFragment rate frag = V.generate (duration_ frag) $
-    quantize . (fromRational (amp_ frag) *) . samplePitchAt rate (pitch_ frag)
+synthesizeFragment :: SampleRate -> (Waveform, Int) -> Vector Sample
+synthesizeFragment rate (wv, n) = V.generate n $
+    quantize . wv . (% rate) . fromIntegral
 
 synthesize :: SampleRate -> Sequencing -> Vector Sample
 synthesize rate ps = V.concat $ map (synthesizeFragment rate) ps
@@ -101,5 +130,8 @@ simpleWavInfo = Info
 
 main :: IO ()
 main = do
-    n <- writeFile simpleWavInfo "/tmp/blah.wav" . toBuffer . synthesize 44100 . interp $ ionian
+    n <- writeFile simpleWavInfo "/tmp/blah.wav" . toBuffer . synthesize 44100 . interp $ melodicMinor 220
     print n
+
+    m <- writeFile simpleWavInfo "/tmp/tone.wav" . toBuffer . synthesizeFragment 44100 . (,44100) . scaleWaveform 0.5 . synthesizeSpectrum . harmonic $ 440
+    print m
